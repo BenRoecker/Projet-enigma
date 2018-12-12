4dae3dde57c9962362b8c722db7ecceab7325b41
Roecker Benjamin <benjamin.roecker@efrei.net>
2018-12-12 16:57:19 +0100

    Update shuffle_alphabet.py

--- shuffle_alphabet.py
+++ shuffle_alphabet.py
@@ -1,12 +1,11 @@
-Alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
+Alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 import random
 
 def shuffle_alphabet():
     answer = []
-    for i in range(len(Alphabet)-1):
+    for i in range(len(Alphabet)):
         lettre = random.choice(Alphabet)
         answer += lettre
         Alphabet.remove(lettre)
     return answer
-
 shuffle_alphabet()
