c343e22a7177b7de8fa58307160a30272fdd1aaa
Roecker Benjamin <benjamin.roecker@efrei.net>
2018-12-12 16:48:55 +0100

    First programm
    First asking programm finish.

--- shuffle_alphabet.py
+++ shuffle_alphabet.py
@@ -1,5 +1,12 @@
 Alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","x","y","z"]
-from random import *
+import random
 
 def shuffle_alphabet():
-    
+    answer = []
+    for i in range(len(Alphabet)-1):
+        lettre = random.choice(Alphabet)
+        answer += lettre
+        Alphabet.remove(lettre)
+    return answer
+
+shuffle_alphabet()
