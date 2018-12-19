672414d49233562b0357d0c2d273d210ec19a02f
Roecker Benjamin <benjamin.roecker@efrei.net>
2018-12-19 15:34:34 +0100

    finish code_enigma.py need verification.

---
+++ code_enigma.py
@@ -0,0 +1,37 @@
+rotor1 = ['b','e','n','g','m','i','d','k','x','c','v','y','a','j','h','w','u','q','p','o','s','r','t','z','f','l']
+rotor2 = ['p','y','d','r','o','i','f','w','l','j','h','b','e','q','x','m','g','s','c','u','t','v','a','k','n','z']
+
+
+
+
+def rea_rotor(rotor,rotor0):
+    while rotor[0] != rotor0:
+        rotor.insert(0,rotor.pop())
+        print(rotor)
+    return rotor
+
+def rotor_bouge(rotor):
+    for i in range (len(rotor)-1,1,-1):
+        if i == len(rotor)-1:
+            rotor[0] = rotor[i]
+        else:
+            rotor[i+1] = rotor[i]
+    return rotor
+def code_enigma(mot,rotor10,rotor20):
+    rea_rotor(rotor1,rotor10)
+    rea_rotor(rotor2,rotor20)
+    solution = ""
+    numbrotor1 = 0
+    for member in mot:
+        newlettre = rotor1[ord(member)-97]
+        new2lettre = rotor2[ord(newlettre)-97]
+        solution += new2lettre
+        rotor_bouge(rotor1)
+        numbrotor1 += 1
+        if numbrotor1 == 26:
+            numbrotor1 = 0
+            rotor_bouge(rotor2)
+    return solution
+
+print(code_enigma("chocolat",'f','z'))
+
