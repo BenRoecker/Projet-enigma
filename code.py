61dc21e1f392deda3e7af87174192f6a79f3a1c4
Roecker Benjamin <benjamin.roecker@efrei.net>
2018-12-19 12:41:29 +0100

    finish code.py

--- code.py
+++ code.py
@@ -1,5 +1,11 @@
 rotor1 = ['b','e','n','g','m','i','d','k','x','c','v','y','a','j','h','w','u','q','p','o','s','r','t','z','f','l']
 rotor2 = ['p','y','d','r','o','i','f','w','l','j','h','b','e','q','x','m','g','s','c','u','t','v','a','k','n','z']
 
-def code():
-    
+def code(mot):
+    solution = ""
+    for member in mot:
+        newlettre = rotor1[ord(member)-97]
+        new2lettre = rotor2[ord(newlettre)-97]
+        solution += new2lettre
+    return solution
+print(code("abcd"))
