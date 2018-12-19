50a0b5b5c7c89f84a746e24891b4b8fb0666130f
Roecker Benjamin <benjamin.roecker@efrei.net>
2018-12-19 12:59:29 +0100

    finish decode

---
+++ decode.py
@@ -0,0 +1,12 @@
+rotor1 = ['b','e','n','g','m','i','d','k','x','c','v','y','a','j','h','w','u','q','p','o','s','r','t','z','f','l']
+rotor2 = ['p','y','d','r','o','i','f','w','l','j','h','b','e','q','x','m','g','s','c','u','t','v','a','k','n','z']
+
+def decode(mot):
+    solution = ""
+    for member in mot:
+        codelettre = chr(rotor2.index(member)+97)
+        code2lettre = chr(rotor1.index(codelettre)+97)
+        solution+= code2lettre
+    return solution
+
+print(decode("yoqf"))
